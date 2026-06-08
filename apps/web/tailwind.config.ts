644638e53b76b7e57db1dda7d89ce20ef344f374
lylian <lyliancelma@MacBookAir-de-lylian.local>
2026-06-08 13:55:20 -0400

    chore: add missing tailwind config for web app

---
+++ apps/web/tailwind.config.ts
@@ -0,0 +1,40 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  darkMode: 'class',
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          background: '#1b263b',
+          accent: '#c5a059',
+        },
+        secondary: {
+          accent: '#8f9779',
+        },
+        content: {
+          background: '#101624',
+        },
+        surface: {
+          elevated: '#151c2b',
+        },
+        text: {
+          primary: '#ffffff',
+          muted: '#cfd5e5',
+          subtle: '#8f9779',
+        },
+        danger: '#ff4b4b',
+        success: '#3ddc84'
+      },
+      fontFamily: {
+        display: ['Playfair Display', 'serif'],
+        body: ['SF Pro', 'Inter', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
+
+export default config;
